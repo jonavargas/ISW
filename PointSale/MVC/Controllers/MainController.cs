e13f8995966e201500ab2fa1870fbd085e2ef539
BryanChaves <bryanchaves.salas19@gmail.com>
2014-07-30 14:43:58 -0600

    interfaz

--- PointSale/MVC/Controllers/MainController.cs
+++ PointSale/MVC/Controllers/MainController.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Data2;
+
+using System.Data.Entity;
 
 namespace MVC.Controllers
 {
     public class MainController : Controller
     {
+        private PointSaleEntities db = new PointSaleEntities();
         //
         // GET: /Main/
 
         public ActionResult Index()
         {
-            return View();
+            var products = db.Products.Include(p => p.Brand1).Include(p => p.Category1);
+            return View(products.ToList());
         }
 
     }
