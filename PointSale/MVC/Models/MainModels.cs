2a79309ec034b7196c2af62b9b7248440856b47f
jonavargas <jonavargas2012@gmail.com>
2014-07-30 23:51:15 -0600

    Filtro funcional en main

---
+++ PointSale/MVC/Models/MainModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class MainModels
+    {
+        public string SearchText { get; set; }
+        public List<Data2.Products> Products { get; set; }
+    }
+}
