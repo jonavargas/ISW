4414191f62cca7b5379326d70dafe3587d4ea28b
jonavargas <jonavargas2012@gmail.com>
2014-08-03 23:58:42 -0600

    Revert "Corrección Nombres"
    This reverts commit c8c34ef026deacc09fb1a425783f7cca7e43b4fd.

--- PointSale/Data3/Products.cs
+++ PointSale/Data3/Products.cs
@@ -31,6 +31,5 @@ namespace Data3
         public virtual Category Category1 { get; set; }
         public virtual ICollection<ProductSuppliers> ProductSuppliers { get; set; }
         public virtual ICollection<WarehouseProducts> WarehouseProducts { get; set; }
-
     }
 }
